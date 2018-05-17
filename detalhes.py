bcf1a7762c8d5fa7bb3ea190fcd25d669a836fa8
Marcos <38471074+marcosbackend@users.noreply.github.com>
2018-05-16 22:58:32 -0300

    Update detalhes.py

--- detalhes.py
+++ detalhes.py
@@ -5,7 +5,7 @@ def show():
 		"\n\n- Você tem a opção de escolher quantas rodadas quer jogar,\nou, continuar jogando infinitamente." +
 		"\n\n- Caso escolha a quantidade de rodadas, será exibido uma mensagem\ncom o número de rodadas restantes." +
 		"\n\n- Caso escolha não escolha o número de rodadas, o jogo irá executar\ninfinitamente, ou até ser parado com alguma tecla não númerica." +
-		"\n\n- Independende do jogo ser ou não contínuo, no final do jogo será\nmostrada uma mensagem com o número total de rodadas jogadas!" +
+		"\n\n- Independente do jogo ser ou não contínuo, no final do jogo será\nmostrada uma mensagem com o número total de rodadas jogadas!" +
 		"\n\n- No final do jogo também mostrará o placar com acertos e \nmostrará se você ganhou, perdeu ou empatou o jogo."
 		"\n\n- Caso queira me adicionar no facebook, o link é: facebook.com/marcosbackend0"
 		)
